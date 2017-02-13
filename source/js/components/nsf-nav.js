e4dbf0d5e0a8dbe203d419cf1ede565b01d69a36
ScottDowne <scott.downe@gmail.com>
2017-02-13 17:18:49 -0500

    Scroll and page reload issues.

--- source/js/components/nsf-nav.js
+++ source/js/components/nsf-nav.js
@@ -57,7 +57,7 @@ var MenuLinks = React.createClass({
 
       // Check to see which is the current nav item.
       links.forEach((element) => {
-        if (scrollY >= element.offsetTop) {
+        if (scrollY >= element.offsetTop - ( window.innerHeight / 2 )) {
           active = element.getAttribute(`id`);
         }
       });
